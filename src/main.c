de4732dada8c13aa95adc99377380de7f6228c9a
Andrey290 <Tunderlel@yandex.ru>
2025-05-06 10:51:43 +0300

    Liminal version before great refactoring. Something works. Something dont.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 //---COMMENTARY---//
 /*
 
+   =^0w0^=
+   1) This is the first version in which everything works in accordance with technical requirement.
+   2) [INFO] outputs are still not deleted.
+   3) !No sorting. 
+   4) !Searching is brutforce.
+   5) !Code may crush on errors.
+   6) !Crushes are not heap-safety.
 
 */
 
@@ -13,7 +20,8 @@
 
 //---CONSTANTS---//
 
-const int MAX_LINE_SIZE = 1024;
+const int  MAX_LINE_SIZE = 1024;
+const char *ARAB_NUMBERS = "0123456789";
 
 //---STRUCTURES---//
 
@@ -40,7 +48,9 @@ ColumnsKeeper count_columns(FILE *csv_file);
 RowsKeeper    count_rows(FILE *csv_file);
 int           parse_csv(Cell **grid, ColumnsKeeper *cols, RowsKeeper *rows, FILE *file);
 int           print_grid(Cell **grid, ColumnsKeeper *cols, RowsKeeper *rows);
-
+int           calculate(Cell *processed_cell, Cell **grid, ColumnsKeeper *cols, RowsKeeper *rows);
+int           geting_row_and_col_separately(int *just_a_number_flag, char *argument_letters, char *argument_numbers, char *argument);
+Cell*         find_cell(argument_letters, int argument_numbers, ColumnsKeeper *cols, RowsKeeper *rows, Cell **grid);
 
 
 int main(int argc, char *argv[]) {
@@ -83,7 +93,7 @@ int main(int argc, char *argv[]) {
 	// PARSE DATA //
 	
 	    // Making Cell massive
-	Cell **grid = (Cell**)malloc(rows.count * sizeof(Cell));
+	Cell **grid = (Cell**)malloc(rows.count * sizeof(Cell*));
 	for(int i = 0; i < rows.count; i++) {
 		grid[i] = (Cell*)malloc(columns.count * sizeof(Cell));
 	}
@@ -95,6 +105,10 @@ int main(int argc, char *argv[]) {
 	} else {
 		printf("[INFO] main : Successfull parsing!\n");
 	}
+	    /*------------------------------------------//
+	    | Here should be FAST SORTING of ROW and COL |
+	    //------------------------------------------*/
+
 	    // Print grid (Printing is also calculating formulas, so programm can stack right here)
 	if(print_grid(grid, &columns, &rows)) {
         	printf("[FAULT] main : Printing error.\n");
@@ -260,14 +274,170 @@ int print_grid(Cell **grid, ColumnsKeeper *cols, RowsKeeper *rows) {
 	for(int i = 0; i < rows->count; i++) {
 		for (int j = 0; j < cols->count; j++) {
 			if (grid[i][j].formula[0] != '=') {
-				grid[i][j].value = (double)atoi(grid[i][j].formula);
+				grid[i][j].value = atof(grid[i][j].formula);
 				grid[i][j].is_resolved = 1;
 				printf("%.2f,", grid[i][j].value);
 			} else {
-				printf("_ne_,");
+				double calculated_value = 0;
+				if(!calculate(grid[i][j], grid, cols, rows)) {
+					grid[i][j].value = calculated_value;
+					grid[i][j].is_resolved = 1;
+					printf("%.2f,", grid[i][j].value);
+				} else {
+					grid[i][j].value = NAN; // Not A Number is mean
+					grid[i][j].is_resolved = 1;
+					printf("NAN,");
+				}
 			}
 		}
 		printf("\n");
 	}	
 	return 0;
+}
+
+// My mistake //
+// int calculate(double *value_to_return, char *formula, Cell **grid, ColumnsKeeper *cols, RowsKeeper *rows) {
+
+int calculate(Cell *processed_cell, Cell **grid, ColumnsKeeper *cols, RowsKeeper *rows) {
+	char   argument_one[MAX_LINE_SIZE  +  32];
+	char   argument_one_letters[  MAX_LINE_SIZE ];
+	char   argument_one_numbers[32];
+	double argument_one_value = 0;
+
+	char   argument_two[MAX_LINE_SIZE  +  32];
+	char   argument_two_letters[  MAX_LINE_SIZE ];
+	char   argument_two_numbers[32];
+	double argument_two_value = 0;
+	
+	char operator[1];
+
+	// Parsing formula
+	
+	//// Getting arguments and operator itself
+
+	int operator_flag = 0;
+	int writing_index = 0;
+	for (int i = 1; ; i++) {
+		if (formula[i] == '+' || formula[i] == '-' || formula[i] == '*') {
+			operator[0] = formula[i];
+			operator_flag++;
+			argument_one[writing_index] = '\0';
+			writing_index = 0;
+		} else if (formula[i] == '\0' || formula[i] == '\n') {
+			if (!operator_flag) {
+				argument_one[writing_index] = '\0';
+			} else {
+				argument_two[writing_index] = '\0';
+			}
+			break;
+		} else {
+			if (!operator_flag) {
+				argument_one[writing_index] = formula[i];
+			} else {
+				argument_two[writing_index] = formula[i];
+			}
+			writing_index++;
+		}
+	}
+
+	//// Geting arg col and arg row separately
+	//// And then finding
+	//// And further computationg cells value
+	
+	////// ARG1
+	
+	int just_a_number_flag = 0;
+
+	geting_row_and_col_separately(&just_a_number_flag, &argument_one_letters, &argument_one_numbers, argument_one);
+
+	if (just_a_number_flag) {
+		argument_one_val = (double)atoi(argument_one_num);
+	} else {
+		Cell *target_cell = find_cell(argument_one_letters, atoi(argument_one_numbers));
+		if (target_cell.is_resolved) {
+			argument_one_val = target_cell.value;
+		} else {
+			calculate(&argument_one_val, target_cell.formula, grid, cols, rows);
+		}	
+	}
+
+	////// ARG2
+	
+	just_a_number_flag = 0;
+	
+	geting_row_and_col_separately(&just_a_number_flag, &argument_two_letters, &argument_two_numbers, argument_two);
+	
+	if (just_a_number_flag) {
+		argument_two_val = (double)atoi(argument_two_num);
+	} else {
+		Cell *target_cell = find_cell(argument_two_letters, atoi(argument_two_numbers));
+		if (target_cell.is_resolved) {
+			argument_two_val = target_cell.value;
+		} else {
+			calculate(&argument_two_val, target_cell.formula, grid, cols, rows);
+		}	
+	}
+
+	// Now computation of the value
+
+	switch (operator[0]) {
+		case "+" : 
+			value_to_return = argument_one_val + argument_two_val;
+		case "-" :
+			value_to_return = argument_one_val + argument_two_val;
+		case "*" :
+			value_to_return = argument_one_val + argument_two_val;
+		case "/" :
+			value_to_return = argument_one_val + argument_two_val;
+		default:
+                        printf("\n[FAULT] calculate/computation : Unexpected operator %s.\n", &operator);
+			return 1;
+	}
+
+	printf("\n[INFO] calculate : %s %s %s.\n", &argument_one, &operator, &argument_two);
+	return 0;
+}
+
+int geting_row_and_col_separately(int *just_a_number_flag, char *argument_letters, char *argument_numbers, char *argument) {
+	windex_let = 0;
+	windex_num = 0;
+	for (int i = 0; ; i++) {
+		// In this realisation numbers are not allowed in columns names
+		if (strchr(ARAB_NUMBERS, argument[i]) != NULL) {
+			if (i == 0) {
+				*just_a_number_flag++;
+				argument_numbers[windex_num] = argument[i];
+				windex_num++;
+			} else {
+				argument_numbers[windex_num] = argument[i];
+				windex_num++;
+			}
+		} else {
+			argument_letters[windex_let] = argument[i];
+			windex_let++;
+			if (argument[i] == '\0') {
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
+Cell* find_cell(argument_letters, int argument_numbers, ColumnsKeeper *cols, RowsKeeper *rows, Cell **grid) {
+		// FIND ROW (Later there should be binary searching)
+		int target_row = 0;
+		for (int i = 0; i < rows.count; i++) {
+			if (rows.ids[i] == argument_numbers) {
+				target_row = i;
+			}
+		}
+	
+		// FIND COL
+		int target_col = 0;
+		for (int i = 0; i < cols.count; i++) {
+			if (strcmp(cols->names[i], argument_letters) = 0) {
+				target_col = i;
+			}
+		}
+	return grid[target_row][target_col];
 }
